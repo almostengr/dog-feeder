42bfae17713b9cd5d829f771418e57ec4bf9781a
Almost Engineer <bitsecondal@gmail.com>
2022-01-06 22:26:44 -0600

    Corrected worker logic

--- Almostengr.PetFeeder.BackEnd/Workers/ScheduleWorker.cs
+++ Almostengr.PetFeeder.BackEnd/Workers/ScheduleWorker.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Almostengr.PetFeeder.BackEnd.Enums;
@@ -15,14 +14,12 @@ namespace Almostengr.PetFeeder.BackEnd.Workers
         private readonly IScheduleService _scheduleService;
         private readonly IFeedingService _feedingService;
         private readonly ISystemSettingService _systemSettingService;
-        private readonly ILogger<ScheduleWorker> _logger;
 
         public ScheduleWorker(IServiceScopeFactory factory, ILogger<ScheduleWorker> logger)
         {
             _scheduleService = factory.CreateScope().ServiceProvider.GetRequiredService<IScheduleService>();
             _feedingService = factory.CreateScope().ServiceProvider.GetRequiredService<IFeedingService>();
             _systemSettingService = factory.CreateScope().ServiceProvider.GetRequiredService<ISystemSettingService>();
-            _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -33,12 +30,12 @@ namespace Almostengr.PetFeeder.BackEnd.Workers
 
                 TimeSpan currentTime = DateTime.Now.TimeOfDay;
 
-                // check if current time matches scheduled feeding time 
                 var schedules = await _scheduleService.GetSchedulesByTimeAsync(currentTime);
 
-                
                 foreach (var schedule in schedules)
                 {
+                    await FeedingTimeAsync(schedule);
+                    
                     if (schedule.FeedingFrequency == FeedingFrequency.Once)
                     {
                         await _scheduleService.DeleteScheduleAsync(schedule.ScheduleId);
@@ -46,24 +43,22 @@ namespace Almostengr.PetFeeder.BackEnd.Workers
                 }
             }
         }
-        
 
-        private async Task FeedingTimeAsync(List<ScheduleDto> schedules)
+        private async Task FeedingTimeAsync(ScheduleDto schedule)
         {
             var feedingMode = await _systemSettingService.GetSystemSettingAsync(nameof(FeedingMode));
 
-            if (schedules.Count > 0 && feedingMode.Value == FeedingMode.Auto.ToString())
+            if (schedule != null && feedingMode.Value == FeedingMode.Auto.ToString())
             {
                 FeedingDto feedingDto = new FeedingDto()
                 {
-                    Amount = schedules[0].FeedingAmount,
+                    Amount = schedule.FeedingAmount,
                     FeedingType = FeedingType.Scheduled
                 };
 
-                _feedingService.CreateFeedingAsync(feedingDto);
+                Task.Run(() => _feedingService.CreateFeedingAsync(feedingDto));
             }
         }
-
 
     }
 }
