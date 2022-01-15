03da72b43fdfed52e157ed6b6e60cf39fc690ff6
Almost Engineer <bitsecondal@gmail.com>
2022-01-15 08:02:55 -0600

    Added button for future use

--- Almostengr.PetFeeder.BackEnd/AppSettings.cs
+++ Almostengr.PetFeeder.BackEnd/AppSettings.cs
@@ -4,6 +4,12 @@ namespace Almostengr.PetFeeder.BackEnd
     {
         public string DatabaseFile { get; set; } = "PetFeeder.db";
         public OutputGpio OutputGpio { get; set; }
+        public InputGpio InputGpio { get; set; }
+    }
+
+    public class InputGpio
+    {
+        public int PottyButton { get; set; } = 24;
     }
 
     public class OutputGpio
