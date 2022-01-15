4beacdfba58d08ce6221f8243122d252607871a6
Almost Engineer <bitsecondal@gmail.com>
2022-01-15 13:32:51 -0600

    Updated default database file name

--- Almostengr.PetFeeder.BackEnd/AppSettings.cs
+++ Almostengr.PetFeeder.BackEnd/AppSettings.cs
@@ -2,21 +2,6 @@ namespace Almostengr.PetFeeder.BackEnd
 {
     public class AppSettings
     {
-        public string DatabaseFile { get; set; } = "PetFeeder.db";
-        public OutputGpio OutputGpio { get; set; }
-        public InputGpio InputGpio { get; set; }
-    }
-
-    public class InputGpio
-    {
-        public int PottyButton { get; set; } = 24;
-    }
-
-    public class OutputGpio
-    {
-        public int LightRelay { get; set; } = 23;
-        public int FoodForwardRelay { get; set; } = 14;
-        public int FoodBackwardRelay { get; set; } = 15;
-        public int WaterValveRelay { get; set; } = 18;
+        public string DatabaseFile { get; set; } = "petfeeder.db";
     }
 }
