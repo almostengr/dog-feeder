28692b79cfd84f7ada3c366253165daee6d88b55
Kenny Robinson <bitsecondal@gmail.com>
2022-07-29 14:13:26 -0500

    Update petfeeder.ino

--- petfeeder.ino
+++ petfeeder.ino
@@ -1,9 +1,17 @@
-int lastFeedMs = 0;
+// https://www.arduino.cc/reference/en/
+
+
 
 const int screenButtonPin = 4;
 const int actionButtonPin = 5;
 const int motorPin = 10;
 const int ledLampPin = 14;
+
+const unsigned long lastFeedScreenInterval = 120000;
+
+unsigned long lastFeedTime = 0;
+unsigned long lastButtonPressTime = 0;
+unsigned int currentScreenNumber = 0;
 
 void setup() {
   pinMode(screenButtonPin, INPUT);
@@ -11,13 +19,21 @@ void setup() {
   // lcd display
   pinMode(motorPin, OUTPUT);
   pinMode(ledLampPin, OUTPUT); 
+  
+  // display message on LCD about software and version
 }
 
 void loop() {
+  if (currentScreenNumber <= 0 || currentScreenNumber > 1)
+  {
+    currentScreenNumber = 1;
+  }
+  
   screenButtonState = digitalRead(screenButtonPin); // check button 1 state 
   
   if (screenButtonState == HIGH) {
-    // do something
+    // if option screen, then save data
+    // go to next screen
   }
   
   // check button 2 state 
@@ -25,4 +41,16 @@ void loop() {
   // update display 
   
   // 
+}
+
+int GoToHomeScreen() {
+  unsigned long currentTime = millis();
+  
+  if ((currentTime - lastButtonPressTime) >= lastFeedScreenInterval){
+    lastButtonPressTime = currentTime;
+    return 1;
+  }
+  
+  return currentScreenNumber;
+  
 }
