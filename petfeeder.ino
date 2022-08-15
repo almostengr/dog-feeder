d940d56ed20f21200b8d604f3c39b3de08c2f6fe
Almost Engineer <bitsecondal@gmail.com>
2022-08-14 21:27:29 -0500

    updated for button press

--- petfeeder.ino
+++ petfeeder.ino
@@ -1,60 +1,191 @@
 // https://www.arduino.cc/reference/en/
 
+#include <LiquidCrystal_I2C.h>
 
+const int SCREEN_BUTTON_PIN = 4;
+const int ACTION_BUTTON_PIN = 5;
+const int MOTOR_PIN = 10;
+const int LIGHT_PIN = 14;
 
-const int screenButtonPin = 4;
-const int actionButtonPin = 5;
-const int motorPin = 10;
-const int ledLampPin = 14;
+const int OFF = 0;
+const int ON = 1;
 
- unsigned long lastFeedScreenInterval = secondsToMilliseconds(120);
+const int PUSHED = 1;
+
+enum MOTOR_DIRECTION
+{
+  FORWARD,
+  REVERSE,
+  M_OFF,
+};
+
+enum DISPLAY
+{
+  D_VERSION,
+  D_LAST_FEED,
+  D_FEEDING,
+  D_DURATION,
+};
+
+enum LIGHT
+{
+  L_ON,
+  L_OFF
+};
+
+LiquidCrystal_I2C lcd_i2c(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
+
+void setup()
+{
+  pinMode(SCREEN_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(ACTION_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(MOTOR_PIN, OUTPUT);
+  pinMode(LIGHT_PIN, OUTPUT);
+
+  lcd_i2c.init();
+  lcd_i2c.backlight();
+}
 
 unsigned long lastFeedTime = 0;
 unsigned long lastButtonPressTime = 0;
-unsigned int currentScreenNumber = 0;
+DISPLAY displayState = D_VERSION;
+LIGHT lightState = L_ON;
+MOTOR_DIRECTION motorDirection = M_OFF;
+unsigned int duration = 2;
+bool actionButtonWasLow = false;
+bool screenButtonWasLow = false;
 
-// state to represent screen that is being displayed
-// transition to represent the phase to go from
+void loop()
+{
+  unsigned long int currentTime = millis();
 
-long secondsToMilliseconds(long seconds){
-  return seconds * 60 * 1000;
+  displayScreen(currentTime);
+  displayLight(currentTime);
 }
 
-void setup() {
-  pinMode(screenButtonPin, INPUT);
-  pinMode(actionButtonPin, INPUT);
-  // lcd display
-  pinMode(motorPin, OUTPUT);
-  pinMode(ledLampPin, OUTPUT); 
-  
-  // display message on LCD about software and version
-}
-
-void loop() {
-  if (currentScreenNumber <= 0 || currentScreenNumber > 1)
+void displayScreen(unsigned long int nowTime)
+{
+  float hoursAgo;
+  switch (displayState)
   {
-    currentScreenNumber = 1;
+    case D_LAST_FEED:
+      hoursAgo = (nowTime - lastFeedTime) / 3600000;
+      lcdDisplayText("LAST FED", hoursAgo + " HOURS AGO");
+
+      if (isScreenButtonPressed())
+      {
+        displayState = D_DURATION;
+      }
+
+      if (isActionButtonPressed()) {
+        displayState = D_FEEDING;
+      }
+      break;
+
+    case D_FEEDING:
+      lcdDisplayText("FEEDING");
+
+      // when feeding duration exceeds state change time
+      break;
+
+
+    case D_DURATION:
+      lcdDisplayText(DURATION, duration);
+
+      if (isScreenButtonPressed())
+      {
+        displayState = D_LAST_FEED;
+        // save data
+      }
+
+      if (isActionButtonPressed())
+      {
+        duration++;
+        if (duration > 9)
+        {
+          duration = 2;
+        }
+      }
+      break;
+
+    default:
+      displayState = D_LAST_FEED;
+      break;
   }
-  
-  screenButtonState = digitalRead(screenButtonPin); // check button 1 state 
-  
-  if (screenButtonState == HIGH) {
-    // if option screen, then save data
-    // go to next screen
-  }
-  
-  // check button 2 state 
-  
-  // update display 
-  
-  // 
 }
 
-void GoToHomeScreen() {
-  unsigned long currentTime = millis();
-  
-  if ((currentTime - lastButtonPressTime) >= lastFeedScreenInterval){
-    lastButtonPressTime = currentTime;
-    currentScreenNumber = 1;
+void displayLight(unsigned long int nowTime)
+{
+  switch (lightState)
+  {
+    case L_OFF:
+      digitalWrite(LIGHT_PIN, OFF);
+
+      if (isScreenButtonPressed() || isActionButtonPressed())
+      {
+        lightState = L_ON;
+        lastButtonPressTime = nowTime;
+      }
+      break;
+
+    default:
+      digitalWrite(LIGHT_PIN, ON);
+
+      if (nowTime - lastButtonPressTime >= 30000)
+      {
+        lightState = L_OFF;
+      }
+      break;
   }
+}
+
+void updateButtonPressTime(unsigned long int time)
+{
+  lastButtonPressTime = time;
+}
+
+bool isActionButtonPressed()
+{
+  // read the state of the pushbutton and set a flag if it is low:
+  if (digitalRead(ACTION_BUTTON_PIN) == LOW)  {
+    actionButtonWasLow = true;
+    // return false;
+  }
+
+  //  return (digitalRead(ACTION_BUTTON_PIN) == PUSHED);
+  // This if statement will only fire on the rising edge of the button input
+  if (digitalRead(ACTION_BUTTON_PIN) == HIGH && actionButtonWasLow)  {
+    actionButtonWasLow = false; // reset the button low flag
+    return true;// Button event here
+  }
+
+  return false;
+}
+
+bool isScreenButtonPressed()
+{
+  //  return (digitalRead(SCREEN_BUTTON_PIN) == PUSHED);
+
+  if (digitalRead(SCREEN_BUTTON_PIN) == LOW)  {
+    screenButtonWasLow = true;    
+  }
+
+  // This if statement will only fire on the rising edge of the button input
+  if (digitalRead(SCREEN_BUTTON_PIN) == HIGH && screenButtonWasLow)  {
+    // reset the button low flag
+    screenButtonWasLow = false;
+
+    // Button event here
+    return true;
+  }
+
+  return false;
+}
+
+void lcdDisplayText(String line1, String line2)
+{
+  lcd_i2c.setCursor(0, 0);
+  lcd_i2c.print(line1);
+  lcd_i2c.setCursor(0, 1);
+  lcd_i2c.print(line2);
 }
